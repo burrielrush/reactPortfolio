5d422c3c6558a5d5498f0006ae6736313c0c6693
Rush Burriel <burrielrush@gmail.com>
2023-07-24 13:59:56 -0600

    added links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,9 +10,15 @@ function Footer() {
   return (
     <div className="footer">
         <div className="socialMedia">
-            <Instagram />
-            <GitHub />
-            <LinkedIn />
+        <a href="https://www.linkedin.com/in/rush-burriel-26a448263/" target="_blank" rel="noopener noreferrer">
+              <LinkedIn />
+            </a>
+            <a href="https://www.instagram.com/rushburriel/" target="_blank" rel="noopener noreferrer">
+              <Instagram /> 
+            </a>
+            <a href="https://github.com/burrielrush" target="_blank" rel="noopener noreferrer">
+              <GitHub />
+            </a>
         </div>
         <p> &copy; 2023</p>
     </div>
